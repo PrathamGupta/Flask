00c64bfe0b2c2b25a81dccc35530e7aac867f4a2
Pratham Gupta <pratham18072@iiitd.ac.in>
2019-06-30 19:02:02 +0530

    added login route

--- auth.py
+++ auth.py
@@ -1,15 +1,30 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
-
+from flask_login import login_user
 from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-
     return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user and not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+    
+    login_user(user)
+
+    return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
 def signup():
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
 
@@ -7,6 +8,8 @@ def index():
     return render_template('index.html')
 
 @main.route('/profile')
+@login_required
 def profile():
-    return render_template('profile.html')
+    name = current_user.name
+    return render_template('profile.html', name= name)
     
